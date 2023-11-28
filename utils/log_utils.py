dcb917c69d57ee8b6397a4a0821ca2943751b148
jdcundiff <jd_cundiff@outlook.com>
2023-11-28 11:41:28 -0600

    pointed log_utils to correct directory

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -8,7 +8,7 @@ def setup_logger(name, log_file, level=logging.INFO):
     log_file_path = os.path.join(log_dir, log_file)
 
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    handler = logging.FileHandler(log_file)
+    handler = logging.FileHandler(log_file_path)
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
