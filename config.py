4a4ff2dab7e0f91cc0e43aeb455674dc53a066ed
jdcundiff <jd_cundiff@outlook.com>
2023-11-28 12:13:17 -0600

    removed config tracking

--- config.py
+++
@@ -1,5 +0,0 @@
-PLATFORM = 'PancakeSwap'  # Change to 'Dogebets' as needed
-API_KEY = 'your_api_key'
-PANCAKESWAP_CONTRACT_ADDRESS = '0x5aF6D33DE2ccEC94efb1bDF8f92Bd58085432d2c'
-DOGEBETS_CONTRACT_ADDRESS = '0x00C3b3E90f2c960D711891B3BD57917c7ACf4B15'
-BSC_HTTP = 'https://bsc-dataseed.binance.org/'
